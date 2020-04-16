22516fd2e5f2f3b8bcac4d4591a09db9e87c88c0
Jamie Lau <36782341+laujamie@users.noreply.github.com>
2020-04-15 20:31:06 -0400

    Complete Cloudflare Challenge (#1)
    * Finish challenge (no bonuses)
    
    * Finish adding handlers to edit HTML content
    
    * Change project type to webpack
    
    * Update gitignore
    
    * Add preliminary cookie checking
    
    * Add cookies for persistent A/B testing
    
    * Add jsdoc

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
+const {
+  LinkHandler,
+  TitleHandler,
+  HeaderHandler,
+  ParagraphHandler
+} = require('./handlers');
+const URI = 'https://cfw-takehome.developers.workers.dev/api/variants';
+
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+  event.respondWith(handleRequest(event.request));
+});
+
 /**
- * Respond with hello worker text
+ * Returns a random integer between the min (inclusive) and
+ * the max (exclusive)
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+/**
+ * Returns a UTC date as a string for the expiration time of a cookie
+ * @returns {string}
+ */
+function getExpiryDate() {
+  const now = new Date();
+  const time = now.getTime();
+  var expireTime = time + 1000 * 36000;
+  now.setTime(expireTime);
+  return now.toUTCString();
+}
+
+/**
+ * Returns a request to one of the two URI variants at random
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  const cookies = request.headers.get('Cookie') || '';
+  const resp = await fetch(URI);
+  const respJson = await resp.json();
+  const variants = respJson.variants;
+  let n = getRandomInt(0, variants.length);
+  if (cookies && cookies.includes(`variant=1`)) {
+    n = 0;
+  } else if (cookies && cookies.includes(`variant=2`)) {
+    n = 1;
+  }
+  const variantResp = await fetch(variants[n]);
+  const editedResp = new HTMLRewriter()
+    .on('a#url', new LinkHandler())
+    .on('title', new TitleHandler(n + 1))
+    .on('h1#title', new HeaderHandler(n + 1))
+    .on('p#description', new ParagraphHandler(n + 1))
+    .transform(variantResp);
+  editedResp.headers.append(
+    'Set-Cookie',
+    `variant=${n + 1};expires=${getExpiryDate()}; path=/`
+  );
+  return editedResp;
 }
